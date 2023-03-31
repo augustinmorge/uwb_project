60dcd49a918604d8d16593902c7af37c04bcc845
Augustin Morge <augustin.morge@ixblue.com>
2023-03-31 10:07:49 +0200

    re-organize files

---
+++ src/kalman_sim/kalman3D.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+import os, sys
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(1, os.path.join(THIS_FOLDER, '../libraries'))
+import toolbox_kalman as tool
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+global dt
+dt = 0.1
+
+def f(X, u):
+    u1, u2, u3, u4, u5, u6 = u.flatten()
+    x, y, z, φ, θ, ψ, vx, vy, vz = X.flatten()
+
+    dp = np.array([[vx], [vy], [vz]])
+    dv = tool.eulermat(np.pi/2-φ, θ, ψ)@np.array([[u1],[u2],[u3]])
+    w = tool.eulerderivative(np.pi/2-φ,θ,ψ)@np.array([[u4], [u5], [u6]]) #Comme le 0 est au nord et tourne dans le sens anti-trigo
+
+    x_dot = np.vstack((dp, w, dv))
+    return x_dot
+
+# sawtooth = lambda x : (2*arctan(tan(x/2)))
+
+L = 20
+def control(x,t,nb=3,display=True):
+
+    x1, x2, x3, x4, x5, x6, x7, x8, x9 = x.flatten()
+
+    K = 1
+    f = 0.1
+
+    w=L*np.array([[np.cos(f*t)],[np.sin(f*nb*t)],[np.sin(f*nb*t + np.pi/4)]])
+    dw=L*np.array([[-f*np.sin(f*t)],[nb*f*np.cos(f*nb*t)], [f*nb*np.cos(f*nb*t + np.pi/4)]])
+    ddw=L*np.array([[-f**2*np.cos(f*t)],[-nb**2*f**2*np.sin(f*nb*t)], [-(f*nb)**2*np.sin(f*nb*t + np.pi/4)]])
+
+    y = np.array([[x1],
+               [x2],
+               [x3]])
+
+    dy = np.array([[x7],
+                [x8],
+                [x9]])
+
+    ddy = w-y + 2*(dw-dy) + ddw
+
+    Ax = tool.eulermat(np.pi/2 - x4, x5, x6)
+    u = np.linalg.inv(Ax)@ddy
+
+    u1 = K*tool.sawtooth((np.pi/2 - np.arctan2(w[1,0]-x2,w[0,0] - x1)) - x4)
+    u2 = K*tool.sawtooth((np.arctan2(w[2,0]-x2,w[0,0] - x1)) - x5)
+    u3 = K*tool.sawtooth((np.arctan2(w[2,0]-x2,w[1,0] - x1)) - x6)
+    u = np.vstack((u1,u2,u3,u))
+
+    if tool.norm(u) > 500:
+        sys.exit()
+
+    if display:
+        plt.scatter(w[0,0],w[1,0], color = 'red', label = 'point to follow')
+        W = lambda f : np.array([L*np.cos(f*t), L*np.sin(nb*f*t)])
+        plt.scatter(W(np.linspace(0,100,1100))[0],W(np.linspace(0,100,1100))[1], s = 0.1, label='trajectory to follow')
+
+    return u
+
+
+def Kalman(xbar, P, u, y, Q, R, F, G, H, all = True):
+    # Prédiction
+    xbar = xbar + dt*f(xbar,u) #F @ xbar + G @ u
+    P = F @ P @ F.T + G @ Q @ G.T
+
+    if all:
+        # Correction
+        ytilde = y - H @ xbar
+        S = H @ P @ H.T + R
+        inv_norm_S = tool.sqrtm(np.linalg.inv(S))@ytilde
+
+        K = P @ H.T @ np.linalg.inv(S)
+        xbar = xbar + K @ ytilde
+        P = P - K @ H @ P
+
+
+        return xbar, P #, ytilde, inv_norm_S
+
+    else:
+        return xbar, P
+
+
+if __name__ == "__main__":
+    global col
+    col = []
+    display_bot = 0
+    GNSS = 0
+
+    if display_bot:
+        ax = tool.init_figure(-L*1.1, L*1.1, -L*1.1, L*1.1)
+    T = []
+
+    import sys
+    from tqdm import tqdm
+    N = 1000
+    PMatrix = np.zeros((N,9**2))
+
+    P = 10 * np.eye(9)
+    X = np.array([[0.1], [0.2], [0.3], [0], [0], [0], [0], [0], [0]])
+    Xhat = X
+
+    sigm_equation = 0 #dt*0.1
+    Q = np.diag([sigm_equation, sigm_equation, sigm_equation, sigm_equation, sigm_equation, sigm_equation])
+
+    ERR = []
+    YTILDE = []
+    ytilde = np.array([[0],[0]])
+
+    wp_detected = False
+
+    for i in tqdm(np.arange(0, N*dt, dt)):
+        # Real state
+        # u = control(X,i)
+        u = np.zeros((6,1))
+        X = X + dt*f(X,u)
+
+        u1, u2, u3, u4, u5, u6 = u.flatten()
+        x, y, z, φ, θ, ψ, vx, vy, vz = X.flatten()
+
+        CBE = tool.eulermat(np.pi/2 - φ, θ, ψ)
+        DBE = tool.eulerderivative(np.pi/2 - φ, θ, ψ)
+        gamma_matrix = np.vstack((np.zeros((6,3)), DBE))
+        v_matrix = np.vstack((np.eye(3), np.zeros((6,3))))
+
+        Fk = np.eye(9) + dt * np.hstack((np.zeros((9,3)), gamma_matrix, v_matrix))
+
+        IO = np.hstack((np.eye(3), np.zeros((3,3))))
+        OCBE = np.hstack((np.zeros((3,3)), CBE))
+        Gk = dt * np.vstack((np.zeros((3,6)),\
+                             IO,\
+                             OCBE))
+
+        sigm_measure = 0.01
+        R_gps = np.diag([sigm_measure, sigm_measure, sigm_measure])
+
+        Y_gps = np.array([[x], [y], [z]])
+        Hk_gps = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0, 0, 0, 0, 0]])
+
+        if i%10 == 0:
+            Xhat, P = Kalman(Xhat, P, u, Y_gps, Q, R_gps, Fk, Gk, Hk_gps, True)
+        else:
+            Xhat, P = Kalman(Xhat, P, u, Y_gps, Q, R_gps, Fk, Gk, Hk_gps, False)
---
+++ src/libraries/toolbox_kalman.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+from roblib import array, cos, sin, inv, sawtooth, scatter, norm, plot, arctan2, vstack, diag, mvnrnd1, sqrtm, eig
+from roblib import Ellipse, eye, log, init_figure, draw_tank, pause, clear, plt
+from roblib import pi, linspace, zeros, sqrt, arange, eulermat, eulerderivative
+from numpy import linalg, random
+import numpy as np
+
+
+def draw_ellipse0(ax, c, Γ, a, col,coledge='black'):  # classical ellipse (x-c)T * invΓ * (x-c) <a^2
+    # draw_ellipse0(ax,array([[1],[2]]),eye(2),a,[0.5,0.6,0.7])
+    A = a * sqrtm(Γ)
+    w, v = eig(A)
+    v1 = array([[v[0, 0]], [v[1, 0]]])
+    v2 = array([[v[0, 1]], [v[1, 1]]])
+    f1 = A @ v1
+    f2 = A @ v2
+    φ = (arctan2(v1[1, 0], v1[0, 0]))
+    α = φ * 180 / 3.14
+    e = Ellipse(xy=c, width=2 * norm(f1), height=2 * norm(f2), angle=α)
+    ax.add_artist(e)
+    e.set_clip_box(ax.bbox)
+
+    e.set_alpha(0.7)
+    e.set_facecolor(col)
+    e.set_edgecolor(coledge)
+
+    # e.set_fill(False)
+    # e.set_alpha(1)
+    # e.set_edgecolor(col)
+
+def draw_ellipse_cov(ax,c,Γ,η, col ='blue',coledge='black'): # Gaussian confidence ellipse with artist
+    #draw_ellipse_cov(ax,array([[1],[2]]),eye(2),0.9,[0.5,0.6,0.7])
+    if (linalg.norm(Γ)==0):
+        Γ=Γ+0.001*eye(len(Γ[1,:]))
+    a=sqrt(-2*log(1-η))
+    draw_ellipse0(ax, c, Γ, a,col,coledge)
+
+def legende(ax):
+    ax.set_title('Filtre de Kalman')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+## Old functions
+
+def g_old(x, Xhat):
+    global Wps
+    x=x.flatten()
+    wp_detected = False
+    H = zeros((1,5))
+    y = zeros((1,1))
+    Beta = []
+    global col
+    for i in range(Wps.shape[1]):
+        a=Wps[:,i].flatten() #wps(i) in (xi,yi)
+        da = a-(x[0:2]).flatten()
+        dist = norm(da)
+        if dist < 15: #On considère qu'on a capté la balise
+            plot(array([a[0],x[0]]),array([a[1],x[1]]),"red",1)
+
+            δ = arctan2(da[1],da[0])
+            Hi = array([[-sin(δ),cos(δ), 0, 0, 0]])
+            yi = [[-sin(δ)*a[0] + cos(δ)*a[1]]]
+            if not wp_detected:
+                    H = Hi; y = yi
+            else:
+                H = vstack((H,Hi)); y = vstack((y,yi))
+            Beta.append(0.011)
+
+            wp_detected = True
+
+    if not wp_detected: col.append('blue')
+    else: col.append('red')
+    Γβ = diag(Beta)
+    if len(Beta) != 0:
+        y = y + mvnrnd1(Γβ)
+
+    return H, y, Γβ, wp_detected
