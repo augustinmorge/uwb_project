446c23dce4478c055309a851e5b52273fe223e58
Augustin Morge <augustin.morge@ixblue.com>
2023-05-17 15:23:56 +0200

    add link to imu, sending message available

---
+++ src/libraries/Arduino/ESP32_UWB_setup_tag/ESP32_UWB_setup_tag.ino
@@ -0,0 +1,186 @@
+// currently tag is module #5
+// The purpose of this code is to set the tag address and antenna delay to default.
+// this tag will be used for calibrating the anchors.
+
+// #include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
+
+// #define SPI_SCK 18
+// #define SPI_MISO 19
+// #define SPI_MOSI 23
+
+// connection pins
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 21;   // spi select pin
+
+// TAG antenna delay defaults to 16384
+// leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
+
+// Configure serial COM RX/TX
+#define DW1000_RX_PIN 25 // Replace with the appropriate RX pin number
+#define DW1000_TX_PIN 26 // Replace with the appropriate TX pin number
+HardwareSerial dwSerial(1); // Use UART 1
+const int baudRate = 115200;
+
+void setup()
+{
+  //Setup serial RX/TX
+  dwSerial.begin(baudRate, SERIAL_8N1, DW1000_RX_PIN, DW1000_TX_PIN);
+
+  //Setup console
+  Serial.begin(115200);
+  delay(1000);
+
+  //init the configuration
+  // SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+// start as tag, do not assign random short address
+
+  //start the module as a tag, do not assign random short address
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+  // DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_SHORTDATA_FAST_LOWPOWER,false);
+  // DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_FAST_LOWPOWER,false);
+  // DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_SHORTDATA_FAST_ACCURACY,false);
+  // DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_FAST_ACCURACY,false);
+  // DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_ACCURACY, false);
+}
+
+// int incomingByte = 0; // for incoming serial data
+void loop()
+{
+  DW1000Ranging.loop();
+
+   /* Time */
+	// Serial.print("timePollSent: ");Serial.println((double)DW1000Ranging.getDistantDevice()->timePollSent.getAsMicroSeconds());
+	
+	// Serial.print("timeRangeSent: ");Serial.println((double)DW1000Ranging.getDistantDevice()->timeRangeSent.getAsMicroSeconds());
+
+  // Serial.print("timeINO: " ); Serial.println(millis());
+
+}
+
+void newRange()
+{ 
+  /* Values that can be displayed */
+  // Serial.print("from: ");
+  // Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  // Serial.print("\t Range: ");
+  // Serial.println(DW1000Ranging.getDistantDevice()->getRange());
+  // Serial.print(" m");
+  // Serial.print("\t RX power: ");
+  // Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+  // Serial.print(" dBm");
+  // Serial.print("\t FP power: ");
+  // Serial.print(DW1000.getFirstPathPower());
+  // Serial.print(" dBm");
+  // Serial.print("\t Quality: ");
+  // Serial.print(DW1000.getReceiveQuality());
+  // Serial.print("\t Timer: ");
+  // long unsigned int timer = millis();
+  // DW1000Time val = DW1000Ranging.getDistantDevice()->timeRangeSent;
+  // double timer = (double) val.getTimestamp() * 1e-11;
+  // Serial.println(timer);
+
+
+
+
+  /* Values that can be sent in binary */
+  uint16_t shortAddress = DW1000Ranging.getDistantDevice()->getShortAddress();
+  int range = (int)(DW1000Ranging.getDistantDevice()->getRange()*100);
+  int RXPower = (int)(DW1000Ranging.getDistantDevice()->getRXPower()*100);
+  int FPPower = (int)(DW1000.getFirstPathPower()*100);
+  int quality = (int)(DW1000.getReceiveQuality()*100);
+  float timerpoll = DW1000Ranging.getDistantDevice()->timePollSent.getAsMicroSeconds();
+  float timersent = DW1000Ranging.getDistantDevice()->timeRangeSent.getAsMicroSeconds();
+  float timerino = millis();
+
+  // Encodage en binaire des données
+  size_t buffer_size = sizeof(shortAddress) + sizeof(range) + sizeof(RXPower) + sizeof(FPPower) + sizeof(quality) + sizeof(timerpoll) + sizeof(timersent) + sizeof(timerino);
+  byte buffer[buffer_size];
+  int offset = 0;
+  memcpy(buffer + offset, &shortAddress, sizeof(shortAddress));
+  offset += sizeof(shortAddress);
+  memcpy(buffer + offset, &range, sizeof(range));
+  offset += sizeof(range);
+  memcpy(buffer + offset, &RXPower, sizeof(RXPower));
+  offset += sizeof(RXPower);
+  memcpy(buffer + offset, &FPPower, sizeof(FPPower));
+  offset += sizeof(FPPower);
+  memcpy(buffer + offset, &quality, sizeof(quality));
+  offset += sizeof(quality);
+  memcpy(buffer + offset, &timerpoll, sizeof(timerpoll));
+  offset += sizeof(timerpoll);
+  memcpy(buffer + offset, &timersent, sizeof(timersent));
+  offset += sizeof(timersent);
+  memcpy(buffer + offset, &timerino, sizeof(timerino));
+  offset += sizeof(timerino);
+
+  // Envoi des données encodées sur le port série
+  Serial.write(buffer, sizeof(buffer));
+
+
+
+
+  /* Values that can be sent in ASCII */
+  // double latitude = 48.5357;
+  // double longitude = 2.0350;
+  // double depth = 10.5; // depth in meters
+  // char lat_hem = (latitude > 0) ? 'N' : 'S';
+  // char lon_hem = (longitude > 0) ? 'E' : 'W';
+  // float beacon_range = DW1000Ranging.getDistantDevice()->getRange();
+  // // Serial.print("range: "); Serial.println(beacon_range);
+  // float beacon_range_std_dev = 0.1;
+  // float age = 0 ; //millis()/1000 - (float)DW1000Ranging.getDistantDevice()->timeRangeSent.getTimestamp()*10
+  // int beacon_id = 80;
+
+  // // LLmm.mmmm to degrees and minutes conversion
+  // int lat_degrees = (int)latitude;
+  // float lat_minutes = (latitude - lat_degrees) * 60.0;
+  // // LLLmm.mmmm to degrees and minutes conversion
+  // int lon_degrees = (int)longitude;
+  // float lon_minutes = (fabs(longitude) - fabs(lon_degrees)) * 60.0;
+
+
+
+  // // construct the ASCII sentence
+  // char sentence[100];
+  // int sentence_length = sprintf(sentence, "$BFLBL,%02d%07.4f,%c,%03d%07.6f,%c,%.3f,%d,%.3f,%.3f,%.3f",
+  //                               lat_degrees, lat_minutes, lat_hem,
+  //                               lon_degrees, lon_minutes, lon_hem,
+  //                               depth, beacon_id, beacon_range, beacon_range_std_dev, age);
+  // // calculate the checksum
+  // Serial.println(sentence);
+  // int checksum = 0;
+  // for (int i = 1; i < sentence_length; i++) {
+  //     checksum ^= sentence[i];
+  // }
+
+  // // append the checksum and end-of-sentence characters
+  // sentence_length += sprintf(sentence + sentence_length, "*%02X\r\n", checksum);
+
+  // // send the sentence over serial
+  // dwSerial.print(sentence);
+
+  // delay(1000);
+
+}
+
+void newDevice(DW1000Device *device)
+{
+  // Serial.print("Device added: ");
+  // Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+  // Serial.print("delete inactive device: ");
+  // Serial.println(device->getShortAddress(), HEX);
+}
---
+++ src/logs/log_ino/read_data.py
@@ -0,0 +1,80 @@
+import serial
+import struct
+
+def receive_data_bits(file, ser):
+    
+    # Tailles en bytes de chaque donnée dans le buffer
+    short_address_size = 2
+    range_size = 4
+    rx_power_size = 4
+    fp_power_size = 4
+    quality_size = 4
+    timer_size_ps = 4
+    timer_size_rs = 4
+    timer_size_ino = 4
+
+    buffer_size = short_address_size + range_size + rx_power_size + fp_power_size + quality_size + timer_size_ino + timer_size_ps + timer_size_rs
+    
+    # Attendre que des données soient disponibles sur la connexion série
+    short_address =  hex(struct.unpack('H', ser.read(2)[:short_address_size])[0])[2:]
+    ids = ['1780', '1781', '1782', '1783']
+    while short_address not in ids: # Attendre de lire l'octet 0xFF
+        short_address =  hex(struct.unpack('H', ser.read(2)[:short_address_size])[0])[2:]
+
+    # Lire les données disponibles depuis la connexion série
+    buffer = ser.read(buffer_size-short_address_size) # Lire le reste du buffer, moins l'octet déjà reçu
+
+    range_value = struct.unpack('i', buffer[:range_size])[0]
+    rx_power = struct.unpack('i', buffer[range_size:range_size+rx_power_size])[0]
+    fp_power = struct.unpack('i', buffer[range_size+rx_power_size:range_size+rx_power_size+fp_power_size])[0]
+    quality = struct.unpack('i', buffer[range_size+rx_power_size+fp_power_size:range_size+rx_power_size+fp_power_size+quality_size])[0]
+    timer_ps = struct.unpack('f', buffer[range_size+rx_power_size+fp_power_size+quality_size:range_size+rx_power_size+fp_power_size+quality_size+timer_size_ps])[0]
+    timer_rs = struct.unpack('f', buffer[range_size+rx_power_size+fp_power_size+quality_size+timer_size_ps:range_size+rx_power_size+fp_power_size+quality_size+timer_size_ps+timer_size_rs])[0]
+    timer_ino = struct.unpack('f', buffer[range_size+rx_power_size+fp_power_size+quality_size+timer_size_ps+timer_size_rs:range_size+rx_power_size+fp_power_size+quality_size+timer_size_ps+timer_size_rs+timer_size_ino])[0]
+    file.write(str(short_address) + ";"+ str(timer_ino) + ";"+ str(range_value) + ";" + str(rx_power) + ";" + str(fp_power) + ";" + str(quality) + ";" + str(timer_ps) + ";" + str(timer_rs) + "\n")
+    file.flush()
+    # Retourner un dictionnaire contenant les données
+    return {
+        'short_address': short_address,
+        'range': range_value,
+        'rx_power': rx_power,
+        'fp_power': fp_power,
+        'quality': quality,
+        'timer_ps': timer_ps,
+        'timer_rs': timer_rs,
+        'timer_ino': timer_ino
+    }
+
+if __name__ == "__main__":
+    # Ouvrir une connexion série sur le port COM5
+    import datetime
+    import os
+    ser = serial.Serial('COM5', 115200)
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    date_str = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    f = open(f"{THIS_FOLDER}/{date_str}_log-all-with-time.csv","w")
+    f.write("Short Address;Timer Ino;Range;RX Power;FP Power;Quality;Timer PS;Timer RS\n")
+    f.flush()
+    i = 0
+    while 1:
+        print(receive_data_bits(f, ser))
+
+    # else:
+    #     i = 0
+    #     # Pour visualiser le temps #
+    #     import numpy as np
+    #     import os
+    #     import matplotlib.pyplot as plt
+
+    #     # Chemin du fichier CSV
+    #     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+    #     # Lecture des données du fichier CSV
+    #     T = np.loadtxt(f"{THIS_FOLDER}/time3.csv")
+
+    #     # Affichage du graphique
+    #     plt.plot(T)
+    #     plt.xlabel("itération")
+    #     plt.ylabel("temps [s]")
+    #     plt.title("Temps envoyé par une ancre")
+    #     plt.show()
